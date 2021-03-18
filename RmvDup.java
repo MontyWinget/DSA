319541002a9ab809ae8ada57835794b0c1f5a34c
Monty Winget <monty.winget@gmail.com>
2021-03-18 23:51:56 +0530

    Remove Duplicates

---
+++ RmvDup.java
@@ -0,0 +1,22 @@
+package com.arrays.java;
+
+public class RmvDup {
+	
+	public static void main(String args[]) {
+		int nums[] = {1, 1, 2};
+		
+		int len = removeDuplicates(nums);
+		
+		for (int i = 0; i < len; i++) {
+		    System.out.print(" " + nums[i]);
+		}
+		
+	}
+	
+	public static int removeDuplicates(int[] arr) {
+        
+		
+		
+		return -1;
+    }
+}
