a7c6f0f040e1df24f427a1b8b673405e5a6ec227
Monty Winget <monty.winget@gmail.com>
2021-05-05 23:09:56 +0530

    Java Hashmap Demo

---
+++ HashMapDemo.java
@@ -0,0 +1,34 @@
+package com.arrays.java;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+
+		HashMap<String, Integer> hash = new HashMap<String, Integer>();
+	      Scanner in = new Scanner(System.in);
+	      int n=in.nextInt();
+	      in.nextLine();
+	      for(int i=0;i<n;i++)
+	      {
+	         String name=in.nextLine();
+	         int phone=in.nextInt();
+	          hash.put(name, phone);
+	         in.nextLine();
+	      }
+	      while(in.hasNext())
+	      {
+	         String s=in.nextLine();
+	          if (hash.containsKey(s)) {
+	              System.out.println(s + "=" + hash.get(s));
+	          } else {
+	              System.out.println("Not found");
+	          }
+	      }
+	      in.close();
+	}
+
+}
